08636a2a3773a30662871fb29ad68778d87ddeff
Meidan Lifshits <meidanlifshits@gmail.com>
2026-07-23 12:14:54 +0300

    solution 3.1
    (cherry picked from commit 8ba59e7e28401066ee012bf1541c1d894237e451)

---
+++ ledsystem/ledssim/BlinkingAnimation.java
@@ -0,0 +1,28 @@
+package ledsystem.ledssim;
+
+import java.awt.Color;
+import ledsystem.utils.StopWatch;
+
+public class BlinkingAnimation implements Animation {
+    private Color activeColor;
+    private StopWatch stopWatch;
+
+    public BlinkingAnimation(Color activeColor) {
+        this.activeColor = activeColor;
+        this.stopWatch = new StopWatch();
+        this.stopWatch.start();
+    }
+
+    @Override
+    public void apply(LedStrip strip) {
+        while (true) {
+            double elapsed = stopWatch.get();
+            if ((int) (elapsed / 2.0) % 2 == 0) {
+                strip.setAll(activeColor);
+            } else {
+                strip.setAll(Color.BLACK);
+            }
+            strip.apply();
+        }
+    }
+}
