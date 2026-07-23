7fe0ac5c89b42ddf98b539e14517bb74fa0e30a7
Meidan Lifshits <meidanlifshits@gmail.com>
2026-07-23 11:14:36 +0300

    solution 1 PR

--- ledsystem/Main.java
+++ ledsystem/Main.java
@@ -3,7 +3,6 @@ package ledsystem;
 import java.awt.Color;
 
 import ledsystem.ledssim.LedSim;
-import ledsystem.ledssim.SolidAnimation;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,7 +10,6 @@ public class Main {
 
         // Initialize the LED simulation with a row layout
         LedSim ledSim = LedSim.createRows(100);
-        SolidAnimation anim = new SolidAnimation(Color.MAGENTA);
 
         // Set all LEDs to a specific color
         ledSim.setAll(Color.CYAN);
@@ -23,9 +21,7 @@ public class Main {
         ledSim.setLed(Color.RED, 5);
 
         // Apply the changes to the simulation
-        anim.apply(ledSim);  
         ledSim.apply();
-
 
         long totalRuntime = System.currentTimeMillis() - startTime;
         System.out.println("Program ended. Total runtime: " + totalRuntime + " ms");
