f316267cf01b06b7c85f48819575f10c5b0a4497
Meidan Lifshits <meidanlifshits@gmail.com>
2026-07-23 11:07:00 +0300

    Update solution 1 code formatting and parameter naming

--- ledsystem/ledssim/SolidAnimation.java
+++ ledsystem/ledssim/SolidAnimation.java
@@ -2,16 +2,15 @@ package ledsystem.ledssim;
 import java.awt.Color;
 
 
-public class SolidAnimation implements Animation{
+public class SolidAnimation implements Animation {
     private Color color;
+
     public SolidAnimation(Color color) {
         this.color = color;
     }
+
     @Override
-    public void apply(LedStrip strip){
+    public void apply(LedStrip strip) {
         strip.setAll(color);
-        
     }
-        
-    
-}
+}
